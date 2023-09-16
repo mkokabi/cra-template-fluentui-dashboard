7d63f5720e041b49d72b45e6697cf06fb1e2cf6c
mkokabi <Mohsen@mokosolution.com>
2023-09-17 07:09:48 +1000

    first commit

---
+++ template/src/App.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+import { PrimaryButton } from '@fluentui/react';
+import './App.css';
+
+const App: FC<any> = ({ instance }) => {
+return (
+  <div>
+    <PrimaryButton>Hello</PrimaryButton>
+  </div>
+)
+}
+
+export default App;
